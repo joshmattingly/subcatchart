3f1273f8b375c0ccffc869aa220c900131f8baf9
Josh Mattingly <josh.mattingly@conagra.com>
2019-03-15 08:48:32 -0500

    Fixed re-sizing issue. Added d3 triangle symbol

--- src/visual.ts
+++ src/visual.ts
@@ -41,7 +41,7 @@ import PrimitiveValue = powerbi.PrimitiveValue;
 import ISelectionId = powerbi.visuals.ISelectionId;
 
 import { VisualSettings } from "./settings";
-import { Primitive } from "d3";
+import { Primitive, symbol, svg } from "d3";
 
 interface ChartViewModel{
     dataPoints: ChartDataPoint[];
@@ -121,6 +121,7 @@ export class Visual implements IVisual {
 
     private host: IVisualHost;
     private svg: d3.Selection<d3.BaseType, any, d3.BaseType, any>;
+    private symbol: d3.Symbol<d3.BaseType, any>;
     private chartContainer: d3.Selection<d3.BaseType, any, d3.BaseType, any>;
     private textValue: d3.Selection<d3.BaseType, any, d3.BaseType, any>;
     private brandContainer: d3.Selection<d3.BaseType, any, d3.BaseType, any>;
@@ -132,34 +133,47 @@ export class Visual implements IVisual {
             .append('svg')
             .classed('subcatChart', true);
 
+        this.symbol = d3.symbol();
+
     }
 
     public update(options: VisualUpdateOptions) {
         let viewModel: ChartViewModel = visualTransform(options, this.host);
-        let width: number = options.viewport.width;
-        let height: number = options.viewport.height;
+
+        let margin = {top: 10, right: 10, bottom: 10, left: 150};
+        let width: number = options.viewport.width - margin.left - margin.right;
+        let height: number = options.viewport.height - margin.top - margin.bottom;
 
         let dataMax = d3.max(viewModel.dataPoints, (d)=>+d.value);
-
+        let increment: number = 3;
         let xScale = d3.scaleLinear()
             .domain([0, dataMax])
             .range([0, width]);
 
-        let yScale = d3.scaleLinear()
-            .domain([0, dataMax])
+        let categoryList = d3.map(viewModel.dataPoints, (d)=> d.category.toString());
+
+        let yScale = d3.scaleBand()
+            .domain(categoryList.keys())
             .range([height, 0])
 
-        this.svg
-        .attr("width", width)
-        .attr("height", height);
+        // clear out the current view
+        this.svg.select(".datapoints").remove();
 
-        this.svg.selectAll("circle")
+        this.svg
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        this.svg
+        .append("g")
+        .attr("class", "datapoints")
+        .selectAll(".points")
         .data(viewModel.dataPoints)
         .enter()
-        .append("circle")
-        .attr("cx", (d) => xScale(+d.value))
-        .attr("cy", (d) => yScale(+d.value))
-        .attr("r", 5)
+        .append("path")
+        .attr("d", this.symbol.size(20).type(d3.symbolTriangle))
+        .attr("transform", (d) => "translate(" + xScale(+d.value) + ", " + (yScale(d.category.toString()) + increment) + ")")
         .attr("fill", "steelblue");
 
     }
